731049155ae813ce054a1a182b53878cc4e93055
steven-kollo <steven.kollo.workspace@gmail.com>
2024-12-27 12:23:21 +0400

    fix long service id

--- flask/static/form-builder.js
+++ flask/static/form-builder.js
@@ -128,7 +128,7 @@ function build_thank_you_page(order_obj, groups_json) {
         let group_name = group.name
         console.log(order[1])
         let title = group.items[order[1][3]].name
-        if (order[1].length == 5) {
+        if (order[1].length == 6) {
             title = group.items[Number(`${order[1][3]}${order[1][4]}`)].name
         }
         
